8203c73d1893a01f5e9679a3ad9102d43ee2250c
Vighnesh Mishra <vighnesh97531@gmail.com>
2023-09-08 10:20:10 +0530

    [Added] Navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar(){
+    return(
+        <section>
+        <div className="relative w-52 h-52 bg-green-500"> 
+            <div className="absolute bottom-2 right-4">
+                heloo
+            </div>
+        </div>
+        </section>
+    )
+}
+
+export default Navbar
