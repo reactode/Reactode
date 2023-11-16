fc396f6e681d705e5044b63304087cba16a5f40c
Shivam Mishra <shivam.r.mishra9120@gmail.com>
2023-11-16 21:16:30 +0530

    ensure repsonsiveness for existing sections

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,33 +1,15 @@
 import Footer from "../components/footer";
 import Hero from "../components/hero";
 import MajorContributors from "../components/majorContributors";
+import WhyReactode from "../components/whyReactode";
 
 export default function Landing() {
   return (
-    <div className="text-white sm:hidden">
-      <div className="h-[100vh]">
+    <div className="text-white flex flex-col gap-y-10 lg:gap-y-20 ">
+      <div className="">
         <Hero />
       </div>
-      <div className="flex flex-col justify-center items-center gap-y-4  ">
-        <div className="flex justify-center items-center gap-x-2 w-[80%] ">
-          <h2 className="font-[Inter] text-2xl">Why Reactode</h2>
-          <img className="w-[10%]" src="/hero/whyR.svg" alt="" />
-        </div>
-        <p className="text-xs text-justify">
-          Choosing a team like Reactode, which collaboratively handles both
-          frontend and backend development, offers several advantages. Firstly,
-          it promotes seamless communication and collaboration between
-          developers, as they are closely aligned in their work. This integrated
-          approach also streamlines the development process, reducing
-          bottlenecks and ensuring quicker delivery of features and updates.
-          Moreover, Reactode's team members can have a more holistic
-          understanding of the entire application, leading to better
-          optimization and improved user experiences. Overall, selecting a team
-          that excels in both frontend and backend development, such as
-          Reactode, can lead to more efficient, cohesive, and high-quality
-          software solutions.
-        </p>
-      </div>
+      <WhyReactode/>
       <MajorContributors/>
       <Footer/>
     </div>
