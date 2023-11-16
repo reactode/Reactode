6d1dcc098d1fc41ee3f1809ab94902201380a52e
Shivam Mishra <shivam.r.mishra9120@gmail.com>
2023-11-16 22:02:16 +0530

    Responsiveness improved

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -5,12 +5,12 @@ export default function Hero() {
     <div className="md:h-[100vh] flex justify-center items-center ">
       <div className="lg:h-[90vh] w-[95vw] bg-[#121831] md:w-[90vw] rounded-2xl md:rounded-3xl p-2">
         <Navbar />
-        <div className="text-white font-[Inter] flex  md:flex-row-reverse justify-center gap-x-[20%] items-center gap-y-8  flex-col mt-4">
-          <img className="max-sm:w-[50vw] w-[25%]   " src="/hero1.svg" alt="" />
-          <div className="flex basis-1/2 flex-col gap-y-8 md:gap-y-16">
+        <div className="text-white font-[Inter] flex  md:flex-row-reverse justify-center gap-x-[20%] items-center gap-y-8  flex-col mt-4 md:p-4">
+          <img className="max-sm:w-[50vw] w-[25%]  " src="/hero1.svg" alt="" />
+          <div className="flex basis-1/2 flex-col justify-center items-center gap-y-8 md:gap-y-16">
             <div className="flex flex-col gay-y-4">
             <h2 className="font-semibold text-xl md:text-3xl">Welcome to REACTODE !!</h2>
-            <p className="text-sm md:text-lg font-normal text-justify">
+            <p className="text-sm md:text-lg font-normal text-justify sm:mt-4">
               Our team is a symphony of creative minds harmonizing to transform
               concepts into captivating digital experiences. We embrace
               challenges as opportunities and thrive on collaborative ingenuity.
@@ -20,7 +20,7 @@ export default function Hero() {
               resonates with the shared heartbeat of innovation
             </p>
             </div>
-            <div className="flex justify-center items-center gap-x-6 text-sm">
+            <div className="flex justify-center items-center gap-x-6 text-sm md:text-xl">
             <button
               className="flex justify-center items-center gap-x-1 p-2"
               style={{
