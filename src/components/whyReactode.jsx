da4c7940d205b789cf53abdc47aed3165787320b
Shivam Mishra <shivam.r.mishra9120@gmail.com>
2023-11-18 11:51:54 +0530

    Improved Why Reactode section

--- src/components/whyReactode.jsx
+++ src/components/whyReactode.jsx
@@ -3,13 +3,15 @@
 export default function WhyReactode() {
   return (
     <div>
-      <div className="flex flex-col justify-center items-center gap-y-4  ">
-        <div className="flex justify-center items-center gap-x-2 w-[80%] ">
-          <h2 className="font-[Inter] text-2xl md:text-4xl">Why Reactode</h2>
+      
+
+<section className="">
+    <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
+        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none  md:text-5xl lg:text-6xl text-white"><div className="flex justify-center items-center gap-x-2  ">
+          <h2 className="font-[Inter] ">Why Reactode</h2>
           <img className="w-[10%] sm:w-[5%]" src="/hero/whyR.svg" alt="" />
-        </div>
-        <p className="text-xs sm:text-sm md:text-lg md:px-40 max-sm:text-justify">
-          Choosing a team like Reactode, which collaboratively handles both
+        </div></h1>
+        <p className="mb-8 text-lg font-normal text-gray-400 lg:text-xl sm:px-16 lg:px-48"> Choosing a team like Reactode, which collaboratively handles both
           frontend and backend development, offers several advantages. Firstly,
           it promotes seamless communication and collaboration between
           developers, as they are closely aligned in their work. This integrated
@@ -20,9 +22,17 @@ export default function WhyReactode() {
           optimization and improved user experiences. Overall, selecting a team
           that excels in both frontend and backend development, such as
           Reactode, can lead to more efficient, cohesive, and high-quality
-          software solutions.
-        </p>
-      </div>
+          software solutions.</p>
+        <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+            
+            <a href="#" className="inline-flex justify-center items-center py-3 px-5 sm:ms-4 text-base font-medium text-center hover:text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 text-white  ">
+                Learn more
+            </a>  
+        </div>
+    </div>
+</section>
+
+     
     </div>
   )
 }
